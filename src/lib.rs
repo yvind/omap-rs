a9c5c353dad7970894046e455736b4a669b15144
oyhj1801 <73776870+oyhj1801@users.noreply.github.com>
2025-01-25 23:15:49 +0100

    fix pub/private issue

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub mod area_object;
 pub mod line_object;
-mod map_geo_traits;
+pub mod map_geo_traits;
 mod map_object;
 pub mod omap;
 pub mod point_object;
@@ -9,7 +9,7 @@ pub mod tag;
 
 pub use self::area_object::AreaObject;
 pub use self::line_object::LineObject;
-use self::map_object::MapObject;
+pub use self::map_object::MapObject;
 pub use self::omap::Omap;
 pub use self::point_object::PointObject;
 pub use self::symbol::Symbol;
