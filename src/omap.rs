1421adaadc1a8ee3b38f3ef446fcb50de1a3e0c3
oyhj1801 <73776870+oyhj1801@users.noreply.github.com>
2025-02-05 20:40:01 +0100

    Added into_objects to get an interator over the objects

--- src/omap.rs
+++ src/omap.rs
@@ -79,6 +79,104 @@ impl Omap {
         }
     }
 
+    pub fn add_object(&mut self, obj: MapObject) {
+        self.objects.push(obj);
+    }
+
+    pub fn write_to_file(
+        self,
+        filename: &OsStr,
+        dir: &Path,
+        bezier_error: Option<f64>,
+    ) -> OmapResult<()> {
+        let mut filepath = PathBuf::from(dir);
+        filepath.push(filename);
+        filepath.set_extension("omap");
+
+        let f = File::create(&filepath)?;
+        let mut f = BufWriter::new(f);
+
+        self.write_header(&mut f)?;
+        self.write_colors_symbols(&mut f)?;
+        self.write_objects(&mut f, bezier_error)?;
+        Self::write_end_of_file(&mut f)?;
+        Ok(())
+    }
+
+    pub fn into_objects(self) -> std::vec::IntoIter<MapObject> {
+        self.objects.into_iter()
+    }
+}
+
+// private functions
+impl Omap {
+    fn write_header(&self, f: &mut BufWriter<File>) -> OmapResult<()> {
+        f.write_all(b"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<map xmlns=\"http://openorienteering.org/apps/mapper/xml/v2\" version=\"9\">\n<notes></notes>\n")?;
+
+        if let Some(epsg) = self.epsg {
+            let geographic_proj = Proj::from_epsg_code(4326)?;
+            let local_proj = Proj::from_epsg_code(epsg)?;
+
+            // transform ref_point to lat/lon
+            let mut geo_ref_point = (self.ref_point.x, self.ref_point.y);
+            transform(&local_proj, &geographic_proj, &mut geo_ref_point)?;
+
+            f.write_all(format!("<georeferencing scale=\"{}\" auxiliary_scale_factor=\"{}\" declination=\"{}\">\
+            <projected_crs id=\"EPSG\"><spec language=\"PROJ.4\">+init=epsg:{}</spec><parameter>{}</parameter>\
+            <ref_point x=\"{}\" y=\"{}\"/></projected_crs><geographic_crs id=\"Geographic coordinates\">\
+            <spec language=\"PROJ.4\">+proj=latlong +datum=WGS84</spec>\
+            <ref_point_deg lat=\"{}\" lon=\"{}\"/></geographic_crs></georeferencing>",
+            self.scale, self.elevation_scale_factor, self.declination, epsg, epsg, self.ref_point.x, self.ref_point.y, geo_ref_point.1, geo_ref_point.0).as_bytes())?;
+        } else {
+            f.write_all(format!("<georeferencing scale=\"{}\"><projected_crs id=\"Local\"><ref_point x=\"{}\" y=\"{}\"/></projected_crs></georeferencing>\n", self.scale, self.ref_point.x, self.ref_point.y).as_bytes())?;
+        }
+
+        Ok(())
+    }
+
+    fn write_colors_symbols(&self, f: &mut BufWriter<File>) -> OmapResult<()> {
+        f.write_all(include_str!("colors.txt").as_bytes())?;
+        match self.scale {
+            Scale::S10_000 => {
+                f.write_all(include_str!("symbols_10.txt").as_bytes())?;
+            }
+            Scale::S15_000 => {
+                f.write_all(include_str!("symbols_15.txt").as_bytes())?;
+            }
+        }
+        Ok(())
+    }
+
+    fn write_objects(self, f: &mut BufWriter<File>, bezier_error: Option<f64>) -> OmapResult<()> {
+        f.write_all(
+            format!(
+                "<parts count=\"1\" current=\"0\">\n<part name=\"map\"><objects count=\"{}\">\n",
+                self.objects.len()
+            )
+            .as_bytes(),
+        )?;
+
+        for object in self.objects.into_iter() {
+            object.write_to_map(
+                f,
+                bezier_error,
+                self.scale,
+                self.grivation,
+                self.combined_scale_factor,
+            )?;
+        }
+
+        f.write_all(b"</objects></part>\n</parts>\n")?;
+        Ok(())
+    }
+
+    fn write_end_of_file(f: &mut BufWriter<File>) -> OmapResult<()> {
+        f.write_all(b"<templates count=\"0\" first_front_template=\"0\">\n<defaults use_meters_per_pixel=\"true\" meters_per_pixel=\"0\" dpi=\"0\" scale=\"0\"/></templates>\n<view>\n")?;
+        f.write_all(b"<grid color=\"#646464\" display=\"0\" alignment=\"0\" additional_rotation=\"0\" unit=\"1\" h_spacing=\"500\" v_spacing=\"500\" h_offset=\"0\" v_offset=\"0\" snapping_enabled=\"true\"/>\n")?;
+        f.write_all(b"<map_view zoom=\"1\" position_x=\"0\" position_y=\"0\"><map opacity=\"1\" visible=\"true\"/><templates count=\"0\"/></map_view>\n</view>\n</barrier>\n</map>")?;
+        Ok(())
+    }
+
     fn scale_factors_and_convergence(epsg: u16, ref_point: Coord) -> OmapResult<(f64, f64, f64)> {
         let geographic_proj = Proj::from_epsg_code(4326)?;
         let local_proj = Proj::from_epsg_code(epsg)?;
@@ -159,96 +257,5 @@ impl Omap {
         let dec = field.declination().get::<radian>();
 
         Ok(dec as f64)
-    }
-
-    pub fn add_object(&mut self, obj: MapObject) {
-        self.objects.push(obj);
-    }
-
-    pub fn write_to_file(
-        self,
-        filename: &OsStr,
-        dir: &Path,
-        bezier_error: Option<f64>,
-    ) -> OmapResult<()> {
-        let mut filepath = PathBuf::from(dir);
-        filepath.push(filename);
-        filepath.set_extension("omap");
-
-        let f = File::create(&filepath)?;
-        let mut f = BufWriter::new(f);
-
-        self.write_header(&mut f)?;
-        self.write_colors_symbols(&mut f)?;
-        self.write_objects(&mut f, bezier_error)?;
-        Self::write_end_of_file(&mut f)?;
-        Ok(())
-    }
-
-    fn write_header(&self, f: &mut BufWriter<File>) -> OmapResult<()> {
-        f.write_all(b"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<map xmlns=\"http://openorienteering.org/apps/mapper/xml/v2\" version=\"9\">\n<notes></notes>\n")?;
-
-        if let Some(epsg) = self.epsg {
-            let geographic_proj = Proj::from_epsg_code(4326)?;
-            let local_proj = Proj::from_epsg_code(epsg)?;
-
-            // transform ref_point to lat/lon
-            let mut geo_ref_point = (self.ref_point.x, self.ref_point.y);
-            transform(&local_proj, &geographic_proj, &mut geo_ref_point)?;
-
-            f.write_all(format!("<georeferencing scale=\"{}\" auxiliary_scale_factor=\"{}\" declination=\"{}\">\
-            <projected_crs id=\"EPSG\"><spec language=\"PROJ.4\">+init=epsg:{}</spec><parameter>{}</parameter>\
-            <ref_point x=\"{}\" y=\"{}\"/></projected_crs><geographic_crs id=\"Geographic coordinates\">\
-            <spec language=\"PROJ.4\">+proj=latlong +datum=WGS84</spec>\
-            <ref_point_deg lat=\"{}\" lon=\"{}\"/></geographic_crs></georeferencing>",
-            self.scale, self.elevation_scale_factor, self.declination, epsg, epsg, self.ref_point.x, self.ref_point.y, geo_ref_point.1, geo_ref_point.0).as_bytes())?;
-        } else {
-            f.write_all(format!("<georeferencing scale=\"{}\"><projected_crs id=\"Local\"><ref_point x=\"{}\" y=\"{}\"/></projected_crs></georeferencing>\n", self.scale, self.ref_point.x, self.ref_point.y).as_bytes())?;
-        }
-
-        Ok(())
-    }
-
-    fn write_colors_symbols(&self, f: &mut BufWriter<File>) -> OmapResult<()> {
-        f.write_all(include_str!("colors.txt").as_bytes())?;
-        match self.scale {
-            Scale::S10_000 => {
-                f.write_all(include_str!("symbols_10.txt").as_bytes())?;
-            }
-            Scale::S15_000 => {
-                f.write_all(include_str!("symbols_15.txt").as_bytes())?;
-            }
-        }
-        Ok(())
-    }
-
-    fn write_objects(self, f: &mut BufWriter<File>, bezier_error: Option<f64>) -> OmapResult<()> {
-        f.write_all(
-            format!(
-                "<parts count=\"1\" current=\"0\">\n<part name=\"map\"><objects count=\"{}\">\n",
-                self.objects.len()
-            )
-            .as_bytes(),
-        )?;
-
-        for object in self.objects.into_iter() {
-            object.write_to_map(
-                f,
-                bezier_error,
-                self.scale,
-                self.grivation,
-                self.combined_scale_factor,
-            )?;
-        }
-
-        f.write_all(b"</objects></part>\n</parts>\n")?;
-        Ok(())
-    }
-
-    fn write_end_of_file(f: &mut BufWriter<File>) -> OmapResult<()> {
-        f.write_all(b"<templates count=\"0\" first_front_template=\"0\">\n<defaults use_meters_per_pixel=\"true\" meters_per_pixel=\"0\" dpi=\"0\" scale=\"0\"/></templates>\n<view>\n")?;
-        f.write_all(b"<grid color=\"#646464\" display=\"0\" alignment=\"0\" additional_rotation=\"0\" unit=\"1\" h_spacing=\"500\" v_spacing=\"500\" h_offset=\"0\" v_offset=\"0\" snapping_enabled=\"true\"/>\n")?;
-        f.write_all(b"<map_view zoom=\"1\" position_x=\"0\" position_y=\"0\"><map opacity=\"1\" visible=\"true\"/><templates count=\"0\"/></map_view>\n</view>\n</barrier>\n</map>")?;
-        Ok(())
     }
 }
