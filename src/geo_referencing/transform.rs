395d440f00df9e381bdaa62782bc3dbfb3018a68
Øyvind Hjermstad <oyvind-hjermstad@hotmail.com>
2026-05-22 18:47:58 +0200

    utility functions for transforming geo_types to map coords

--- src/geo_referencing/transform.rs
+++ src/geo_referencing/transform.rs
@@ -1,4 +1,4 @@
-use geo_types::Coord;
+use geo_types::{Coord, LineString, Point, Polygon};
 
 use super::GeoRef;
 
@@ -13,8 +13,8 @@ pub struct Transform {
 }
 
 impl Transform {
-    /// Convert projected (CRS) coordinates to map coordinates.
-    pub fn to_map_coords(&self, proj_coord: Coord) -> Coord {
+    /// Convert a [Coord] in projected (CRS) coordinates to map coordinates.
+    pub fn to_map_coordinates(&self, proj_coord: Coord) -> Coord {
         let (x, mut y) = ((proj_coord - self.proj_center) / self.scale_factor).x_y();
 
         let x_r = x * self.cos - y * self.sin;
@@ -23,8 +23,35 @@ impl Transform {
         Coord { x: x_r, y } + self.map_center
     }
 
-    /// Convert map coordinates to projected (CRS) coordinates.
-    pub fn to_proj_coords(&self, map_coord: Coord) -> Coord {
+    /// Convert a [Polygon] in projected (CRS) coordinates to map coordinates.
+    pub fn to_map_polygon(&self, proj_polygon: Polygon) -> Polygon {
+        let (ext, ints) = proj_polygon.into_inner();
+
+        let map_ext = self.to_map_linestring(ext);
+        let map_ints = ints
+            .into_iter()
+            .map(|l| self.to_map_linestring(l))
+            .collect::<Vec<_>>();
+
+        Polygon::new(map_ext, map_ints)
+    }
+
+    /// Convert a [LineString] in projected (CRS) coordinates to map coordinates.
+    pub fn to_map_linestring(&self, proj_linestring: LineString) -> LineString {
+        proj_linestring
+            .into_inner()
+            .into_iter()
+            .map(|c| self.to_map_coordinates(c))
+            .collect::<LineString>()
+    }
+
+    /// Convert a [Point] in projected (CRS) coordinates to map coordinates.
+    pub fn to_map_point(&self, proj_point: Point) -> Point {
+        self.to_map_coordinates(proj_point.0).into()
+    }
+
+    /// Convert a [Coord] in map coordinates to projected (CRS) coordinates.
+    pub fn to_projected_coordinates(&self, map_coord: Coord) -> Coord {
         let (x, mut y) = ((map_coord - self.map_center) * self.scale_factor).x_y();
 
         // we want to rotate other way so flip the signs of the sins
@@ -32,6 +59,33 @@ impl Transform {
         y = -x * self.sin + y * self.cos;
 
         Coord { x: x_r, y } + self.proj_center
+    }
+
+    /// Convert a [Polygon] in map coordinates to projected (CRS) coordinates.
+    pub fn to_projected_polygon(&self, map_polygon: Polygon) -> Polygon {
+        let (ext, ints) = map_polygon.into_inner();
+
+        let map_ext = self.to_projected_linestring(ext);
+        let map_ints = ints
+            .into_iter()
+            .map(|l| self.to_projected_linestring(l))
+            .collect::<Vec<_>>();
+
+        Polygon::new(map_ext, map_ints)
+    }
+
+    /// Convert a [LineString] in map coordinates to projected (CRS) coordinates.
+    pub fn to_projected_linestring(&self, map_linestring: LineString) -> LineString {
+        map_linestring
+            .into_inner()
+            .into_iter()
+            .map(|c| self.to_projected_coordinates(c))
+            .collect::<LineString>()
+    }
+
+    /// Convert a [Point] in map coordinates to projected (CRS) coordinates.
+    pub fn to_projected_point(&self, proj_point: Point) -> Point {
+        self.to_projected_coordinates(proj_point.0).into()
     }
 
     pub(super) fn from_geo_ref(geo_ref: &GeoRef) -> Self {
